d1c1133d0f9c5d0d9f1595e6156d4acfc18c3bdb
Shinare <wahapaka@gmail.com>
2015-03-03 22:06:56 +0000

    HOme page

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -3,9 +3,77 @@
 @section('head')
     <link rel="stylesheet" href="{{ asset('css/photoswipe.css') }}">
     <link rel="stylesheet" href="{{ asset('css/default-skin.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/gallery.css') }}">
 @endsection
 
 @section('content')
+
+        <h2>First gallery:</h2>
+
+        <div class="my-simple-gallery" itemscope itemtype="http://schema.org/ImageGallery">
+
+            <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                <a href="https://farm3.staticflickr.com/2567/5697107145_a4c2eaa0cd_o.jpg" itemprop="contentUrl" data-size="1024x1024">
+                    <img src="https://farm3.staticflickr.com/2567/5697107145_3c27ff3cd1_m.jpg" itemprop="thumbnail" alt="Image description" />
+                </a>
+                <figcaption itemprop="caption description">Image caption  1</figcaption>
+
+            </figure>
+
+            <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                <a href="https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg" itemprop="contentUrl" data-size="964x1024">
+                    <img src="https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_m.jpg" itemprop="thumbnail" alt="Image description" />
+                </a>
+                <figcaption itemprop="caption description">Image caption 2</figcaption>
+            </figure>
+
+            <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                <a href="https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg" itemprop="contentUrl" data-size="1024x683">
+                    <img src="https://farm7.staticflickr.com/6175/6176698785_7dee72237e_m.jpg" itemprop="thumbnail" alt="Image description" />
+                </a>
+                <figcaption itemprop="caption description">Image caption 3</figcaption>
+            </figure>
+
+            <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                <a href="https://farm6.staticflickr.com/5023/5578283926_822e5e5791_b.jpg" itemprop="contentUrl" data-size="1024x768">
+                    <img src="https://farm6.staticflickr.com/5023/5578283926_822e5e5791_m.jpg" itemprop="thumbnail" alt="Image description" />
+                </a>
+                <figcaption itemprop="caption description">Image caption 4</figcaption>
+            </figure>
+
+
+        </div>
+
+        <h2>Second gallery:</h2>
+
+        <div class="my-simple-gallery" itemscope itemtype="http://schema.org/ImageGallery">
+
+
+
+            <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                <a href="https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg" itemprop="contentUrl" data-size="964x1024">
+                    <img src="https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_m.jpg" itemprop="thumbnail" alt="Image description" />
+                </a>
+                <figcaption itemprop="caption description">Image caption 2.1</figcaption>
+            </figure>
+
+            <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                <a href="https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg" itemprop="contentUrl" data-size="1024x683">
+                    <img src="https://farm7.staticflickr.com/6175/6176698785_7dee72237e_m.jpg" itemprop="thumbnail" alt="Image description" />
+                </a>
+                <figcaption itemprop="caption description">Image caption 2.2</figcaption>
+            </figure>
+
+            <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+                <a href="https://farm6.staticflickr.com/5023/5578283926_822e5e5791_b.jpg" itemprop="contentUrl" data-size="1024x768">
+                    <img src="https://farm6.staticflickr.com/5023/5578283926_822e5e5791_m.jpg" itemprop="thumbnail" alt="Image description" />
+                </a>
+                <figcaption itemprop="caption description">Image caption 2.3</figcaption>
+            </figure>
+
+
+        </div>
+<!-- Core swipe FUNCTION ###################################################################################################################-->
         <!-- Root element of PhotoSwipe. Must have class pswp. -->
         <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
 
@@ -81,31 +149,196 @@
 
 @section('end')
     <script>
-        var pswpElement = document.querySelectorAll('.pswp')[0];
+    var initPhotoSwipeFromDOM = function(gallerySelector) {
 
-        // build items array
-        var items = [
-    {
-        src: 'https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg',
-        w: 964,
-        h: 1024
-        },
-    {
-        src: 'https://placekitten.com/1200/900',
-        w: 1200,
-        h: 900
+        // parse slide data (url, title, size ...) from DOM elements
+        // (children of gallerySelector)
+        var parseThumbnailElements = function(el) {
+        var thumbElements = el.childNodes,
+        numNodes = thumbElements.length,
+        items = [],
+        figureEl,
+        childElements,
+        linkEl,
+        size,
+        item;
+
+        for(var i = 0; i < numNodes; i++) {
+
+
+        figureEl = thumbElements[i]; // <figure> element
+
+        // include only element nodes
+        if(figureEl.nodeType !== 1) {
+        continue;
         }
-        ];
+
+    linkEl = figureEl.children[0]; // <a> element
+
+    size = linkEl.getAttribute('data-size').split('x');
+
+    // create slide object
+    item = {
+        src: linkEl.getAttribute('href'),
+        w: parseInt(size[0], 10),
+        h: parseInt(size[1], 10)
+        };
+
+
+
+    if(figureEl.children.length > 1) {
+        // <figcaption> content
+        item.title = figureEl.children[1].innerHTML;
+        }
+
+    if(linkEl.children.length > 0) {
+        // <img> thumbnail element, retrieving thumbnail url
+        item.msrc = linkEl.children[0].getAttribute('src');
+        }
+
+    item.el = figureEl; // save link to element for getThumbBoundsFn
+    items.push(item);
+    }
+
+    return items;
+    };
+
+    // find nearest parent element
+    var closest = function closest(el, fn) {
+        return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+        };
+
+    // triggers when user clicks on thumbnail
+    var onThumbnailsClick = function(e) {
+        e = e || window.event;
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+        var eTarget = e.target || e.srcElement;
+
+        var clickedListItem = closest(eTarget, function(el) {
+        return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
+        });
+
+    if(!clickedListItem) {
+        return;
+        }
+
+
+    // find index of clicked item
+    var clickedGallery = clickedListItem.parentNode,
+    childNodes = clickedListItem.parentNode.childNodes,
+    numChildNodes = childNodes.length,
+    nodeIndex = 0,
+    index;
+
+    for (var i = 0; i < numChildNodes; i++) {
+            if(childNodes[i].nodeType !== 1) {
+            continue;
+            }
+
+        if(childNodes[i] === clickedListItem) {
+            index = nodeIndex;
+            break;
+            }
+        nodeIndex++;
+        }
+
+
+
+        if(index >= 0) {
+            openPhotoSwipe( index, clickedGallery );
+            }
+        return false;
+        };
+
+        // parse picture index and gallery index from URL (#&pid=1&gid=2)
+        var photoswipeParseHash = function() {
+            var hash = window.location.hash.substring(1),
+            params = {};
+
+        if(hash.length < 5) {
+                return params;
+                }
+
+            var vars = hash.split('&');
+            for (var i = 0; i < vars.length; i++) {
+                    if(!vars[i]) {
+                    continue;
+                    }
+                var pair = vars[i].split('=');
+                if(pair.length < 2) {
+                        continue;
+                        }
+                    params[pair[0]] = pair[1];
+                    }
+
+                    if(params.gid) {
+                        params.gid = parseInt(params.gid, 10);
+                        }
+
+                    if(!params.hasOwnProperty('pid')) {
+            return params;
+        }
+        params.pid = parseInt(params.pid, 10);
+        return params;
+    };
+
+    var openPhotoSwipe = function(index, galleryElement, disableAnimation) {
+        var pswpElement = document.querySelectorAll('.pswp')[0],
+            gallery,
+            options,
+            items;
+
+        items = parseThumbnailElements(galleryElement);
 
         // define options (if needed)
-        var options = {
-            // optionName: 'option value'
-            // for example:
-            index: 0 // start at first slide
-            };
+        options = {
+            index: index,
 
-        // Initializes and opens PhotoSwipe
-        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+            // define gallery index (for URL)
+            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+            getThumbBoundsFn: function(index) {
+                // See Options -> getThumbBoundsFn section of docs for more info
+                var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+                    pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                    rect = thumbnail.getBoundingClientRect();
+
+                return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+            },
+
+            // history & focus options are disabled on CodePen
+            // remove these lines in real life:
+           history: false,
+           focus: false
+
+        };
+
+        if(disableAnimation) {
+            options.showAnimationDuration = 0;
+        }
+
+        // Pass data to PhotoSwipe and initialize it
+        gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
         gallery.init();
+    };
+
+    // loop through all gallery elements and bind events
+    var galleryElements = document.querySelectorAll( gallerySelector );
+
+    for(var i = 0, l = galleryElements.length; i < l; i++) {
+        galleryElements[i].setAttribute('data-pswp-uid', i+1);
+        galleryElements[i].onclick = onThumbnailsClick;
+    }
+
+    // Parse URL and open gallery if it contains #&pid=3&gid=1
+    var hashData = photoswipeParseHash();
+    if(hashData.pid > 0 && hashData.gid > 0) {
+        openPhotoSwipe( hashData.pid - 1 ,  galleryElements[ hashData.gid - 1 ], true );
+    }
+};
+
+// execute above function
+initPhotoSwipeFromDOM('.my-simple-gallery');
     </script>
 @endsection
