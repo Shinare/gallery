03d22e00f37645ce1c5ceeb5658c8ba9b5380dc1
Shinare <wahapaka@gmail.com>
2015-03-03 22:48:22 +0000

    Add amazon files

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -13,30 +13,30 @@
         <div class="my-simple-gallery" itemscope itemtype="http://schema.org/ImageGallery">
 
             <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-                <a href="https://farm3.staticflickr.com/2567/5697107145_a4c2eaa0cd_o.jpg" itemprop="contentUrl" data-size="1024x1024">
-                    <img src="https://farm3.staticflickr.com/2567/5697107145_3c27ff3cd1_m.jpg" itemprop="thumbnail" alt="Image description" />
+                <a href="https://s3-eu-west-1.amazonaws.com/www.kacprzyk.co.uk/gallery/20140705--59.jpg" itemprop="contentUrl" data-size="1024x1024">
+                    <img src="https://s3-eu-west-1.amazonaws.com/www.kacprzyk.co.uk/gallery/thumbnails/20140705--59.jpg" itemprop="thumbnail" alt="Image description" />
                 </a>
                 <figcaption itemprop="caption description">Image caption  1</figcaption>
 
             </figure>
 
             <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-                <a href="https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg" itemprop="contentUrl" data-size="964x1024">
-                    <img src="https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_m.jpg" itemprop="thumbnail" alt="Image description" />
+                <a href="https://s3-eu-west-1.amazonaws.com/www.kacprzyk.co.uk/gallery/20140705--62.jpg" itemprop="contentUrl" data-size="964x1024">
+                    <img src="https://s3-eu-west-1.amazonaws.com/www.kacprzyk.co.uk/gallery/thumbnails/20140705--62.jpg" itemprop="thumbnail" alt="Image description" />
                 </a>
                 <figcaption itemprop="caption description">Image caption 2</figcaption>
             </figure>
 
             <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-                <a href="https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg" itemprop="contentUrl" data-size="1024x683">
-                    <img src="https://farm7.staticflickr.com/6175/6176698785_7dee72237e_m.jpg" itemprop="thumbnail" alt="Image description" />
+                <a href="https://s3-eu-west-1.amazonaws.com/www.kacprzyk.co.uk/gallery/_MG_0102.jpg" itemprop="contentUrl" data-size="1024x683">
+                    <img src="https://s3-eu-west-1.amazonaws.com/www.kacprzyk.co.uk/gallery/thumbnails/_MG_0102.jpg" itemprop="thumbnail" alt="Image description" />
                 </a>
                 <figcaption itemprop="caption description">Image caption 3</figcaption>
             </figure>
 
             <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-                <a href="https://farm6.staticflickr.com/5023/5578283926_822e5e5791_b.jpg" itemprop="contentUrl" data-size="1024x768">
-                    <img src="https://farm6.staticflickr.com/5023/5578283926_822e5e5791_m.jpg" itemprop="thumbnail" alt="Image description" />
+                <a href="https://s3-eu-west-1.amazonaws.com/www.kacprzyk.co.uk/gallery/_MG_0339.jpg" itemprop="contentUrl" data-size="1024x768">
+                    <img src="https://s3-eu-west-1.amazonaws.com/www.kacprzyk.co.uk/gallery/thumbnails/_MG_0339.jpg" itemprop="thumbnail" alt="Image description" />
                 </a>
                 <figcaption itemprop="caption description">Image caption 4</figcaption>
             </figure>
