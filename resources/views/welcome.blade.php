b937d556ab587cb130d7a8272f0b5575a6d2626a
Shinare <wahapaka@gmail.com>
2015-03-13 22:53:46 +0000

    Teamspeak

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
 			</div>
             <a>Click me</a>
             <pre><?php
-            $output = shell_exec('ls ../../ -lart 2>&1');
+            $output = shell_exec('ls ../../../ -lart 2>&1');
             echo $output ?></pre>
 		</div>
 @endsection
