de3730a5d4d6390a42f62b81193ea1d0d8bcc6de
Shinare <wahapaka@gmail.com>
2015-03-13 22:45:12 +0000

    Teamspeak

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
 			</div>
             <a>Click me</a>
             <pre><?php
-            $output = shell_exec('ls /home/ec2-user/ -lart');
+            $output = shell_exec('sudo ls /home/ec2-user/ -lart');
             echo $output ?></pre>
 		</div>
 @endsection
