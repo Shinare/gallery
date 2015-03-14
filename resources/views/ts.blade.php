3f68b0bf970870b9126ebfd3754accda0cabb366
Shinare <wahapaka@gmail.com>
2015-03-14 02:11:04 +0000

    Teamspeak

--- resources/views/ts.blade.php
+++ resources/views/ts.blade.php
@@ -46,7 +46,10 @@
             echo $output; ?></pre>
             <form action="{{ URL::route('ts') }}" method="get" class="form-signin">
 
-            <input type="submit" name="command" value="Start" class="btn btn-lg btn-primary btn-block">
+            <input type="submit" name="command" value="Status" class="btn btn-lg btn-primary btn-block">
+                <input type="submit" name="command" value="Restart" class="btn btn-lg btn-success btn-block">
+                    <input type="submit" name="command" value="Restart" class="btn btn-lg btn-warning btn-block">
+                        <input type="submit" name="command" value="Stop" class="btn btn-lg btn-danger btn-block">
             </form>
 		</div>
 @endsection
