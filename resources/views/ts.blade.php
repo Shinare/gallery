96646529d604f050e89f1b4d675069811598048e
Shinare <wahapaka@gmail.com>
2015-03-14 02:16:05 +0000

    Teamspeak

--- resources/views/ts.blade.php
+++ resources/views/ts.blade.php
@@ -38,8 +38,8 @@
 @section('content')
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<div class="title">Team Speak</div>
+				<div class="quote">Controll panel</div>
 			</div>
             <a>Click me</a>
             <pre><?php
