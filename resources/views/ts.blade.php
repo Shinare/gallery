e192454a700d25758ec3443a0b22f67d26274f6d
Shinare <wahapaka@gmail.com>
2015-03-16 20:40:22 +0000

    Laravel

--- resources/views/ts.blade.php
+++ resources/views/ts.blade.php
@@ -33,12 +33,14 @@
             <div class="quote">Controll panel</div>
 
             <pre>{{$output}}</pre>
-            <form action="{{ url('ts') }}" method="get" class="form-signin col-md">
+            <div class="col-md-2"></div>
+            <form action="{{ url('ts') }}" method="get" class="form-signin col-md-8">
                 <input type="submit" name="command" value="Status" class="btn btn-lg btn-primary btn-block">
                 <input type="submit" name="command" value="Start" class="btn btn-lg btn-success btn-block">
                 <input type="submit" name="command" value="Restart" class="btn btn-lg btn-warning btn-block">
                 <input type="submit" name="command" value="Stop" class="btn btn-lg btn-danger btn-block">
             </form>
+            <div class="col-md-2"></div>
         </div>
     </div>
 </div>
