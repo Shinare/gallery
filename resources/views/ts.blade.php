39fe92799897027d24863fac7d352be5f564fcf6
Shinare <wahapaka@gmail.com>
2015-03-16 18:05:17 +0000

    Laravel

--- resources/views/ts.blade.php
+++ resources/views/ts.blade.php
@@ -4,25 +4,12 @@
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
 		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-			}
-
 			.container {
 				text-align: center;
 				display: table-cell;
 				vertical-align: middle;
                 font-weight: 100;
                 font-family: 'Lato';
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
 			}
 
 			.title {
@@ -36,11 +23,12 @@
 		</style>
 @endsection
 @section('content')
-		<div class="container">
-			<div class="content">
-				<div class="title">Team Speak</div>
-				<div class="quote">Controll panel</div>
-			</div><br>
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="title">Team Speak</div>
+            <div class="quote">Controll panel</div>
+
             <pre>{{$output}}</pre>
             <form action="{{ url('ts') }}" method="get" class="form-signin">
                 <input type="submit" name="command" value="Status" class="btn btn-lg btn-primary btn-block">
@@ -48,5 +36,7 @@
                 <input type="submit" name="command" value="Restart" class="btn btn-lg btn-warning btn-block">
                 <input type="submit" name="command" value="Stop" class="btn btn-lg btn-danger btn-block">
             </form>
-		</div>
+        </div>
+    </div>
+</div>
 @endsection
