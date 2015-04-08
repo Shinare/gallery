31117e40108311441979ce7796d6c360652caa15
Shinare <wahapaka@gmail.com>
2015-04-08 04:32:41 +0100

    From IDE

--- resources/views/gallery-add.blade.php
+++ resources/views/gallery-add.blade.php
@@ -4,7 +4,7 @@
     @if($mover=!"")
         {{$mover}}
     @endif
-    <form enctype="multipart/form-data" method="post" type="file" action="URL('/gallery/add')">
+    <form enctype="multipart/form-data" method="post" type="file" action="{{URL('/gallery/add')}}">
         Chose your file:
         <input name="uploaded_file" type="file"><br>
         <input type="submit" value="Upload!">
