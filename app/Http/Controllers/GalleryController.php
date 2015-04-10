1efae730f9a3ba0fccc6ecf02d5ea610305e8e2a
Shinare <wahapaka@gmail.com>
2015-04-10 04:44:29 +0100

    From IDE

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -178,7 +178,7 @@ class GalleryController extends Controller {
 
     public function showGallery(){
 
-        $photos = Photo::where('galleries_id','=',1)->first()->title;
+        $photos = Photo::where('galleries_id','=',1);
 
             foreach($photos as $photo){
                 echo $photo->title;
