fbb7f05bac5b03a74218bfdb1754cfeede40c066
Shinare <wahapaka@gmail.com>
2015-04-10 03:34:50 +0100

    From IDE

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -89,7 +89,7 @@ Management system
         <div class="my-simple-gallery" itemscope itemtype="http://schema.org/ImageGallery">
 
 
-            {{showMeGallery();}}
+            {{showMeGallery()}}
 
 
         </div>
