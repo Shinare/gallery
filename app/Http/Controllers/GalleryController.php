3c3900c230c16b96ce2e0d7bde22a233daad6408
Shinare <wahapaka@gmail.com>
2015-04-10 04:41:24 +0100

    From IDE

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -178,7 +178,7 @@ class GalleryController extends Controller {
 
     public function showGallery(){
 
-        $photos = Photo::where('galleries_id','=',1);
+        $photos = Photo::where('galleries_id','=',1)->first()->title;
 
 echo "herro";
 
