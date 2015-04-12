043dde9a312db14c5dde48eaa3b7ab82fc9e2eab
Shinare <wahapaka@gmail.com>
2015-04-12 03:16:45 +0100

    From IDE

--- public/upload/js/main.js
+++ public/upload/js/main.js
@@ -18,7 +18,7 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        url: 'autoadd'
+        url: '../upload/server/php/index.php'
     });
 
     // Enable iframe cross-domain access via redirect option:
